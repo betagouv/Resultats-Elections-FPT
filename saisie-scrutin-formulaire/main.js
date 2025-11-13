c8269d7460fc82680ff83f40eddcfc9d2247dee2
Charline-L <charline.laporte@beta.gouv.fr>
2025-11-13 14:32:12 +0100

    Lint

--- saisie-scrutin-formulaire/main.js
+++ saisie-scrutin-formulaire/main.js
@@ -1,26 +1,28 @@
 /* SETUP */
-grist.ready({ requiredAccess: 'full', columns: [
-  'ColumnName',
-  {
-    name: "NombreInscrits",
-    description: "Nombre d'inscrit",
-  },
-  {
-    name: "AbsenceCandidat",
-    description: "Colonne s'il n'y a pas de candidat",
-  },
-  {
-    name: "Resultats",
-    description: "Colonnes qui comptabilisent les votes",
-    allowMultiple: true 
-  }, 
-  {
-    name: "Syndicats",
-    description: "Colonnes qui comptabilisent les voix des syndicats",
-    allowMultiple: true 
-  },
-]});
-
+grist.ready({
+  requiredAccess: 'full',
+  columns: [
+    'ColumnName',
+    {
+      name: 'NombreInscrits',
+      description: "Nombre d'inscrit",
+    },
+    {
+      name: 'AbsenceCandidat',
+      description: "Colonne s'il n'y a pas de candidat",
+    },
+    {
+      name: 'Resultats',
+      description: 'Colonnes qui comptabilisent les votes',
+      allowMultiple: true,
+    },
+    {
+      name: 'Syndicats',
+      description: 'Colonnes qui comptabilisent les voix des syndicats',
+      allowMultiple: true,
+    },
+  ],
+})
 
 /* VAR */
 const namesElement = document.querySelectorAll('[data-name="collectivite"]')
@@ -47,37 +49,41 @@ let abscenceInput = null
 let currentRecord = null
 let isSaving = false
 
-
 /* FUNCTIONS */
 const getHtmlType = (gristName) => {
-  if (gristName === "Int" || gristName === "Numeric") return "number"
-  if (gristName === "Bool") return "checkbox"
-  if (gristName === "Attachments") return "file"
-  return "text"
+  if (gristName === 'Int' || gristName === 'Numeric') return 'number'
+  if (gristName === 'Bool') return 'checkbox'
+  if (gristName === 'Attachments') return 'file'
+  return 'text'
 }
 
 /* COLUMNS */
 const getTableColumnsInfos = async () => {
-  if (tableColumnsInfos.length > 0 ) return
+  if (tableColumnsInfos.length > 0) return
   const tableName = await grist.getSelectedTableId()
   const allTables = await grist.docApi.fetchTable('_grist_Tables')
   const tableId = allTables.id[allTables.tableId.indexOf(tableName)]
   const allGristColumns = await grist.docApi.fetchTable('_grist_Tables_column')
   let index = 0
-  tableColumnsInfos = allGristColumns.parentId.reduce(function (filtered, currentValue){
-    if (currentValue === tableId ) filtered.push({
-      label: allGristColumns.label[index],
-      description: allGristColumns.description[index],
-      colId: allGristColumns.colId[index],
-      type: allGristColumns.type[index],
-    })
+  tableColumnsInfos = allGristColumns.parentId.reduce(function (
+    filtered,
+    currentValue
+  ) {
+    if (currentValue === tableId)
+      filtered.push({
+        label: allGristColumns.label[index],
+        description: allGristColumns.description[index],
+        colId: allGristColumns.colId[index],
+        type: allGristColumns.type[index],
+      })
     index++
     return filtered
-  }, [])
+  },
+  [])
 }
 
 const getColumnsInfos = (column) => {
-  return tableColumnsInfos.filter(col => column.includes(col.colId))
+  return tableColumnsInfos.filter((col) => column.includes(col.colId))
 }
 
 const getColumnInfo = (column) => {
@@ -87,7 +93,10 @@ const getColumnInfo = (column) => {
 
 const generateForm = () => {
   const inscrits = getColumnsInfos(nombreInscritsMapped)
-  const inscritInput = generateInputText(inscrits[0], getHtmlType(inscrits[0].type))
+  const inscritInput = generateInputText(
+    inscrits[0],
+    getHtmlType(inscrits[0].type)
+  )
   inscritInput.setAttribute('id', 'inscrits')
   requiredInputs.appendChild(inscritInput)
 
@@ -96,21 +105,23 @@ const generateForm = () => {
   absenceInput.setAttribute('id', 'absence')
   requiredInputs.appendChild(absenceInput)
 
-  for (let i = 0; i < resultatsMapped.length; i++){
+  for (let i = 0; i < resultatsMapped.length; i++) {
     const coloneInfo = getColumnInfo(resultatsMapped[i])
     const type = getHtmlType(coloneInfo.type)
     const isTypeText = type === 'number' || type === 'text'
-    const input = isTypeText ? generateInputText(coloneInfo, type, 'fr-col-12')
-    : generateInputCheckbox(coloneInfo, 'fr-col-12')
+    const input = isTypeText
+      ? generateInputText(coloneInfo, type, 'fr-col-12')
+      : generateInputCheckbox(coloneInfo, 'fr-col-12')
     votesInputs.appendChild(input)
   }
 
   const syndicats = getColumnsInfos(syndicatsMapped)
-  for (let i = 0; i < syndicats.length; i++){
+  for (let i = 0; i < syndicats.length; i++) {
     const type = getHtmlType(syndicats[i].type)
     const isTypeText = type === 'number' || type === 'text'
-    const input = isTypeText ? generateInputText(syndicats[i], type, 'fr-col-6')
-    : generateInputCheckbox(syndicats[i], 'fr-col-6')
+    const input = isTypeText
+      ? generateInputText(syndicats[i], type, 'fr-col-6')
+      : generateInputCheckbox(syndicats[i], 'fr-col-6')
     syndicatInputs.appendChild(input)
   }
 }
@@ -124,7 +135,7 @@ const generateInputText = (column, type, classToAdd) => {
   label.textContent = column.label
   label.classList.add('fr-label')
   label.setAttribute('for', column.colId)
-  
+
   const description = document.createElement('span')
   description.classList.add('fr-hint-text')
   description.textContent = column.description
@@ -155,7 +166,7 @@ const generateInputCheckbox = (column, classToAdd) => {
   input.setAttribute('type', 'checkbox')
   input.setAttribute('id', column.colId)
   input.setAttribute('name', column.colId)
-  
+
   const label = document.createElement('label')
   label.setAttribute('for', column.colId)
   label.textContent = column.label
@@ -175,32 +186,31 @@ const generateInputCheckbox = (column, classToAdd) => {
 const getFormValues = () => {
   let values = {}
   const allInputs = formElement.querySelectorAll('input')
-  for(let i = 0; i< allInputs.length; i++) {
+  for (let i = 0; i < allInputs.length; i++) {
     const input = allInputs[i]
     const name = input.getAttribute('name')
-    const isCheckboxe = input.getAttribute('type') === "checkbox"
+    const isCheckboxe = input.getAttribute('type') === 'checkbox'
     values[name] = isCheckboxe ? input.checked : input.value
   }
   return values
 }
 
 const formatValue = (value) => {
-  const isCheckboxe = value === "on" || value === "off"
-  if (isCheckboxe) return value === "on"
+  const isCheckboxe = value === 'on' || value === 'off'
+  if (isCheckboxe) return value === 'on'
   return value
 }
 
 const prefillForm = () => {
   const inputs = formElement.querySelectorAll('input')
-  for (let i = 0; i < inputs.length; i++ ){
+  for (let i = 0; i < inputs.length; i++) {
     const input = inputs[i]
     const name = input.getAttribute('name')
     const value = currentRecord[name]
-    if (typeof value === "boolean") input.checked = value
+    if (typeof value === 'boolean') input.checked = value
     else input.setAttribute('value', value)
   }
 }
-
 
 const resetView = () => {
   formElement.classList.remove('fr-hidden')
@@ -211,7 +221,6 @@ const resetView = () => {
   prefillForm()
   checkAbsence()
 }
-
 
 /* DISABLED FIELDS */
 const generateAbsence = () => {
@@ -226,7 +235,10 @@ const watchAbsence = () => {
 }
 
 const checkAbsence = () => {
-  inputsToUpdate = [...votesInputs.querySelectorAll('input'), ...syndicatInputs.querySelectorAll('input')]
+  inputsToUpdate = [
+    ...votesInputs.querySelectorAll('input'),
+    ...syndicatInputs.querySelectorAll('input'),
+  ]
   abscenceInput = document.querySelector('#absence input')
   toggleInputs(abscenceInput.checked)
 }
@@ -240,7 +252,7 @@ const enabledInputs = () => {
   for (let i = 0; i < inputsToUpdate.length; i++) {
     const input = inputsToUpdate[i]
     const colName = input.getAttribute('name')
-    input.removeAttribute("disabled")
+    input.removeAttribute('disabled')
     input.value = currentRecord[colName]
   }
 }
@@ -248,8 +260,8 @@ const enabledInputs = () => {
 const disabledInputs = () => {
   for (let i = 0; i < inputsToUpdate.length; i++) {
     const input = inputsToUpdate[i]
-    input.value = input.type === "checkbox" ? 'off' : ''
-    input.setAttribute("disabled", true)
+    input.value = input.type === 'checkbox' ? 'off' : ''
+    input.setAttribute('disabled', true)
   }
 }
 
@@ -263,7 +275,6 @@ const displayMessage = (type) => {
   formElement.classList.add('fr-hidden')
 }
 
-
 /* SUBMIT */
 formElement.addEventListener('submit', async (event) => {
   event.preventDefault()
@@ -272,10 +283,10 @@ formElement.addEventListener('submit', async (event) => {
   try {
     grist.selectedTable.update({
       id: rowIdSelected,
-      fields: formValues
+      fields: formValues,
     })
     displayMessage('success')
-  } catch(e) {
+  } catch (e) {
     displayMessage('error')
   }
 })
@@ -292,14 +303,13 @@ grist.onRecord(async (record, mapping) => {
   absenceCandidatMapped = mapping['AbsenceCandidat']
   resultatsMapped = mapping['Resultats']
   syndicatsMapped = mapping['Syndicats']
-  namesElement.forEach(name => {
+  namesElement.forEach((name) => {
     name.textContent = record[columnNameMapped]
   })
   if (!isSaving) resetView()
   isSaving = false
   window.scrollTo(0, 0)
-});
-
+})
 
 /* INIT */
 const initView = async () => {
