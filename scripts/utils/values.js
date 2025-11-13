bf05cabd2300e5e48a49d208b03c2c71cd39f59c
Charline-L <charline.laporte@beta.gouv.fr>
2025-11-13 10:36:41 +0100

    Corrige l'erreur pour les valeurs censurées

--- scripts/utils/values.js
+++ scripts/utils/values.js
@@ -1,5 +1,5 @@
 const isInString = (string, value) => {
-  if (typeof value !== "string") return false // Censored values
+  if (typeof string !== 'string') return false // Censored values
   const stringClean = string.toLowerCase()
   const valueLower = value.toLowerCase()
   return stringClean.indexOf(valueLower) >= 0
