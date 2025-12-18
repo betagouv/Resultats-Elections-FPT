7ed8fbe6461922f3201e85e663cf04c9b8c242d2
Charline-L <charline.laporte@beta.gouv.fr>
2025-12-18 15:26:51 +0100

    Création de la nouvelle vue générique

---
+++ vue-fiche/README.md
@@ -0,0 +1,26 @@
+# Vue fiche DSFR
+
+A faire : 
+- [x] Création de la nouvelle vue
+- [x] Corriger l'affichage du titre la vue  
+- [x] Passer tous les sélecteurs avec des data-attribute  
+- [ ] Passer la suppression en action
+
+
+À intégrer : 
+- [ ] Saisie de résultats  
+- [ ] Saisie de données genrées  
+- [ ] Validation de résultats  
+- [ ] Carto collectivité  
+- [ ] Carto scrutin  
+
+Vérifications :
+- [ ] Sémantique 
+- [ ] Accessibilité  
+- [ ] Responsive
+- [ ] Renommer les colonnes pour les rendre génériques
+
+Améliorations : 
+- [ ] Récupérer le nom des colonnes avec un autre endpoint api
+- [ ] Passer le badge à l'intérieur des "data" en mode DSFR
+- [ ] Changer la colonne du badge pour avoir un objet "DSFR" qui intègre le type du badge
---
+++ vue-fiche/main.js
@@ -0,0 +1,183 @@
+/* IMPORTS */
+import gristUtils from '../scripts/utils/grist.js'
+import valuesUtils from '../scripts/utils/values.js'
+import Modal from '../scripts/classes/Modal.js'
+
+/* VAR */
+const emptyCardElement = document.querySelector('[data-js="card-empty"]')
+const filledCardElement = document.querySelector('[data-js="card-filled"]')
+const titleElements = document.querySelectorAll('[data-js="title"]')
+const badgeElement = document.querySelector('[data-js="badge"]')
+const dataElement = document.querySelector('[data-js="data"]')
+const errorsElement = document.querySelector('[data-js="errors"]')
+const modalElement = document.querySelector('[data-js="modal"]')
+const modalButtonElement = modalElement.querySelector('[data-js="modal-button"]')
+const modalTitleElement = modalElement.querySelector('[data-js="modal-title"]')
+const modal = new Modal({
+  container: modalElement,
+})
+
+let titleMapped = null
+let badgeMapped = null
+let dataMapped = null
+let errorsMapped = null
+let actionMapped = null
+let currentRecord = null
+let tableColumnsInfos = []
+let actionSubmit = null
+
+/* GRIST */
+grist.ready({
+  requiredAccess: 'full',
+  columns: [
+    {
+      name: 'title',
+      description: 'Titre de ma carte',
+    },
+    {
+      name: 'badge',
+      description: 'Badge',
+      optional: true,
+    },
+    {
+      name: 'data',
+      description: 'Données saisies',
+      allowMultiple: true,
+    },
+    {
+      name: 'errors',
+      description: 'Contrôles de cohérences',
+      allowMultiple: true,
+      optional: true,
+    },
+    {
+      name: 'action',
+      description: 'Action',
+      optional: true,
+    },
+  ],
+})
+
+grist.onRecord(async (record) => {
+  // Le curseur s'est déplacé
+  currentRecord = record
+  await needsColumnInfos()
+  displayContent()
+  window.scrollTo(0, 0)
+})
+
+grist.onRecords(async (table, mapping) => {
+  // Les données dans la table ont changé.
+  titleMapped = mapping['title']
+  badgeMapped = mapping['badge']
+  dataMapped = mapping['data']
+  errorsMapped = mapping['errors']
+  actionMapped = mapping['action']
+  await needsColumnInfos()
+})
+
+/* CONTENT */
+const displayContent = () => {
+  resetCard()
+  const status = currentRecord[badgeMapped]
+  fillTitle()
+  if (status === 'À renseigner') {
+    filledCardElement.classList.add('fr-hidden')
+    emptyCardElement.classList.remove('fr-hidden')
+  } else {
+    fillCard()
+    filledCardElement.classList.remove('fr-hidden')
+    emptyCardElement.classList.add('fr-hidden')
+  }
+
+  // Action
+  if (actionMapped) {
+    // Créer un bouton
+    modalButtonElement.textContent = currentRecord[actionMapped].button
+    modalTitleElement.textContent = currentRecord[actionMapped].description
+    modalElement.classList.remove('fr-hidden')
+    actionSubmit = modalElement.querySelector('#submit')
+    // Active un bouton ou non
+    if (currentRecord[actionMapped].isDisabled) {
+      actionSubmit.classList.add('fr-hidden')
+    } else {
+      actionSubmit.classList.remove('fr-hidden')
+      actionSubmit.addEventListener('click', () => {
+        submitAction([currentRecord[actionMapped].action])
+      })
+    }
+  }
+}
+
+const resetCard = () => {
+  dataElement.replaceChildren()
+  errorsElement.replaceChildren()
+}
+
+const fillTitle = () => {
+  titleElements.forEach(title => {
+    title.textContent = currentRecord[titleMapped]
+  })
+}
+
+const fillCard = () => {
+
+  // Badge
+  const status = currentRecord[badgeMapped].toLowerCase()
+  badgeElement.classList.remove('fr-badge--error')
+  badgeElement.classList.remove('fr-badge--success')
+  badgeElement.textContent = status
+  if (['complet', 'validé'].includes(status))
+    badgeElement.classList.add('fr-badge--success')
+  else if (['incomplet', 'doublon', 'à valider'].includes(status))
+    badgeElement.classList.add('fr-badge--error')
+
+
+  // Informations
+  for (let i = 0; i < dataMapped.length; i++) {
+    const li = document.createElement('li')
+    const p = document.createElement('p')
+    p.classList.add('fr-mb-1v')
+    const columnInfos = gristUtils.getColumnInfos(
+      dataMapped[i],
+      tableColumnsInfos
+    )
+    const prettyValue = valuesUtils.prettify(currentRecord[dataMapped[i]])
+    const prettyLabel = columnInfos.label
+    p.textContent = `${prettyLabel} : ${prettyValue}`
+    li.appendChild(p)
+    dataElement.appendChild(li)
+  }
+
+  // Erreurs
+  for (let i = 0; i < errorsMapped.length; i++) {
+    const error = currentRecord[errorsMapped[i]]
+    if (!error) continue
+    const alertError = generateAlertError(error)
+    errorsElement.appendChild(alertError)
+  }
+}
+
+/* ACTION */
+const submitAction = async (actions) => {
+  const updatedRecord = await grist.docApi.applyUserActions(actions)
+  if (updatedRecord) modal.closeDialog()
+}
+
+/* ERROR */
+const generateAlertError = (content) => {
+  const li = document.createElement('li')
+  li.classList.add('fr-alert', 'fr-alert--error')
+  const p = document.createElement('p')
+  p.classList.add('fr-alert__title')
+  p.textContent = content
+  li.appendChild(p)
+  return li
+}
+
+/* COLUMNS INFOS */
+const needsColumnInfos = async () => {
+  if (tableColumnsInfos.length === 0) {
+    tableColumnsInfos = await gristUtils.getTableColumnsInfos()
+  }
+}
