bb235bc357cbd9b0bb88d1a1c7d6b4f1df4eebb5
Charline-L <charline.laporte@beta.gouv.fr>
2026-01-14 17:09:37 +0100

    Création d'un composant Grist pour gérer le paramétrage avec l'api

---
+++ shared/components/GristContainer.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props = defineProps(['columns'])
+const emit = defineEmits(['update:record', 'update:records'])
+
+
+/* GRIST */
+grist.ready({
+  requiredAccess: 'full',
+  columns: props.columns || []
+})
+
+grist.onRecord((record) => {
+  emit('update:record', record)
+})
+
+grist.onRecords((table, mapping) => {
+  emit('update:records', {table, mapping})
+})
+
+</script>
+<template>
+  <main>
+    <slot />
+  </main>  
+</template>
