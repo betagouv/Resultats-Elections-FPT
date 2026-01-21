56bd86e34925b375f649dd578ecef9927ae471ea
Charline-L <charline.laporte@beta.gouv.fr>
2026-01-21 17:36:55 +0100

    Ajoute un message si le chargement est en cours

--- apps/vue-fiche/src/App.vue
+++ apps/vue-fiche/src/App.vue
@@ -181,7 +181,7 @@ const getExcelType = (type) => {
             <StatusBadge :label="currentRecord[badgeMapped]" />
           </div>
           <div class="fr-col-6 fr-grid-row fr-grid-row--right">
-            <DsfrButton v-if="showDownloadButton" secondary label="Enregistrer les données (.xlsx)" @click="downloadExcel" />
+            <DsfrButton v-if="showDownloadButton" secondary :label="isDownloadingFile ? 'Enregistrement en cours...' : 'Enregistrer les données (.xlsx)'" @click="downloadExcel" />
           </div>
         </div>
         <ul class="fr-pl-0 fr-mb-3w app-list--unstyled">
