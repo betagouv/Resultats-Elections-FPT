4a18116ca1a8d2f20e2aa1aad4b384c44d2648e1
Charline-L <charline.laporte@beta.gouv.fr>
2026-02-26 15:46:22 +0100

    Fige la prod V0.12 pour l'audit

---
+++ audit-rgaa/scripts/utils/values.js
@@ -0,0 +1,26 @@
+const removeSpecialChars = (string) => {
+  return string
+    .trim()
+    .toLowerCase()
+    .replaceAll("-", " ")
+    .replaceAll("'", " ")
+    .replaceAll("  ", " ") // Remove double spaces
+    .normalize("NFD") // Convert string to unicode normalize : needed for accent replacement
+    .replace(/[\u0300-\u036f]/g, "") // Remove accents : all chars between unicode U+0300 to U+036F
+}
+
+const isInString = (string, value) => {
+  if (typeof string !== 'string') return false // Censored values
+  const stringClean = removeSpecialChars(string)
+  const valueClean = removeSpecialChars(value)
+  return stringClean.indexOf(valueClean) >= 0
+}
+
+const prettify = (value) => {
+  if (value === true) return 'oui'
+  if (value === false) return 'non'
+  if (value === null || value.length === 0) return 'non renseigné'
+  return value
+}
+
+export default { isInString, prettify }
