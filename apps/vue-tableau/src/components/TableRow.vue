a0e878a63ff3c45df7778c2b10dcb0d744de5eb5
Charline-L <charline.laporte@beta.gouv.fr>
2026-04-29 13:26:17 +0200

    Création d'un composant pour gérer affichage dynamique des ligne

---
+++ apps/vue-tableau/src/components/TableRow.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue'
+import IconCheck from '@shared/components/IconCheck.vue'
+
+const props = defineProps(['cell', 'colKey', 'isSelected', 'isFirstColumn'])
+const emit = defineEmits(['selectRow'])
+
+const checkboxeCursor = ref(props.isSelected)
+const clickCheckboxeCursor = () => {
+  emit('selectRow', props.cell.id)
+}
+</script>
+
+<template>
+  <div v-if="isFirstColumn" class="table-row__checkbox">
+    <DsfrCheckbox :model-value="checkboxeCursor" :label="cell.value" :small="true" @update:modelValue="clickCheckboxeCursor" :readonly="isSelected"/>
+  </div>
+  <DsfrBadge v-else-if="cell.isDSFRBadge" :label="cell.value.text" :type="cell.value.type" />
+  <p v-else-if="cell.type === 'Bool'" class="app-flex-center">
+    <IconCheck v-if="cell.value" class="vue-tableau__icon-check fr-text-title--blue-france" />
+  </p>
+  <DsfrTag v-else-if="cell.type.indexOf('Ref:') > -1" :label="cell.value" />
+  <ul v-else-if="cell.hasMultipleValues">
+    <li v-for="value in cell.value">
+      <p class="fr-mb-0">{{ value }}</p>
+    </li>
+  </ul>
+  <p class="fr-mb-0" v-else>{{ cell.value }}</p>
+</template>
+
+<style>
+.table-row__checkbox .fr-fieldset__element {
+  margin-bottom: 0 !important;
+}
+</style>
