b9a5f730b257d537208ae3f6e15a68ea462db7d0
Charline-L <charline.laporte@beta.gouv.fr>
2026-06-19 16:57:14 +0200

    Un badge peut ne pas avoir de label

--- shared/components/StatusBadge.vue
+++ shared/components/StatusBadge.vue
@@ -3,6 +3,7 @@ import { computed } from 'vue'
 const props = defineProps(['label'])
 
 const type = computed(() => {
+  if (!props.label) return ''
   const labelLower = props.label.toLowerCase()
   if (labelLower === 'complet' || labelLower === 'validé') return 'success'
   if (labelLower === 'incomplet' || labelLower === 'à valider' || labelLower === 'doublon' || labelLower === 'champs manquant(s)') return 'error'
