fc39bc4ea35be4d0b388c03ddf24dd7c53ff69d3
Charline-L <charline.laporte@beta.gouv.fr>
2026-07-30 12:03:53 +0200

    Ré-organise le panneau pour ajouter l'espace des informations de la table

--- shared/components/GristContainer.vue
+++ shared/components/GristContainer.vue
@@ -52,19 +52,35 @@ const updateCursorPos = (cursorPos) => {
   grist.setCursorPos({ rowId: cursorPos })
 }
 defineExpose({updateCursorPos})
+
+/* Table column infos cache */
+const tableColumnInfos = ref(null)
+const saveTableColumnInfos = () => {
+  console.log('saveTableColumnInfos')
+}
 </script>
 <template>
   <main class="grist-container">
     <aside v-if="configurationIsOpened" class="grist-container__configuration fr-p-2w">
       <h2>Panneau de configuration</h2>
-      <p v-if="configurationEmpty">Aucune configuration disponible</p>
-      <div class="fr-mb-2w" v-else>
-        <label>{{ configuration.label }} :
-          <input v-model="configurationInput" type="text" :name="configuration.name" />
-        </label>
-      </div>
-      <p>Après avoir cliquer sur "Fermer le panneau de configuration", vous devrez cliquer sur le bouton "Enregistrer" de Grist pour que les modifications soient prises en compte.</p>
       <button @click="closeConfiguration">Fermer le panneau de configuration</button>
+      <p class="fr-text--xs fr-mt-1w">Après avoir cliquer sur "Fermer le panneau de configuration", vous devrez cliquer sur le bouton "Enregistrer" de Grist pour que les modifications soient prises en compte.</p>
+      <hr>
+      <div class="fr-my-2w">
+        <h3>1. Configuration de la vue personnalisée</h3>
+        <p v-if="configurationEmpty">Aucune configuration disponible</p>
+        <div class="fr-mb-2w" v-else>
+          <label>{{ configuration.label }} :
+            <input v-model="configurationInput" type="text" :name="configuration.name" />
+          </label>
+        </div>
+      </div>
+      <div class="fr-my-2w">
+        <h3>2. Informations de la table</h3>
+        <p>Les informations de la table sont enregistrées pour être utilisées dans la vue personnalisée.</p>
+        <button @click="saveTableColumnInfos">Actualiser les informations</button>
+        <pre>{{ tableColumnInfos || 'Aucune information de la table enregistrée' }}</pre>
+      </div>
     </aside>
     <slot />
   </main>  
@@ -79,6 +95,7 @@ defineExpose({updateCursorPos})
   height: 100%;
   background-color: var(--background-contrast-blue-france);
   z-index: 9;
+  overflow-y: scroll;
 }
 
 .grist-container__configuration input,
