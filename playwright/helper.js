41beb5e3746864e670ca222ef348a5c858c58a59
Charline-L <charline.laporte@beta.gouv.fr>
2026-08-20 17:20:13 +0200

    Refacto

--- playwright/helper.js
+++ playwright/helper.js
@@ -1,17 +1,8 @@
-const docId = process.env.GRIST_DOC_ID
+import grist from './grist.js'
 const viewAsPref = "?aclAsUser_=dgcl.test.pref%40beta.gouv.fr"
 const viewAsAdmin = "?aclAsUser_=dgcl.test.admin%40beta.gouv.fr"
 
-const pagesID = {
-  "accueil": "10"
-}
-
-const getUrl = (page) => {
-  return `https://grist.numerique.gouv.fr/o/dgcl/${docId}/p/${pagesID[page]}`
-}
-
-const getWidget = (page) => {
-  return page.locator('iframe').contentFrame().locator('iframe').contentFrame()
-}
+const getUrl = (page) => `https://grist.numerique.gouv.fr/o/dgcl/${grist.docID}/p/${grist.pagesID[page]}`
+const getWidget = (page) => page.locator('iframe').contentFrame().locator('iframe').contentFrame()
 
 export default { getUrl, getWidget, viewAsPref, viewAsAdmin }
