71ff537417f017eb968d4ff08dd797df83934b58
Charline-L <charline.laporte@beta.gouv.fr>
2026-08-25 12:12:49 +0200

    Ajoute le nom du document dans l'url et précise quel type de widget est récupéré

--- playwright/helper.js
+++ playwright/helper.js
@@ -2,7 +2,7 @@ import grist from './grist.js'
 const viewAsPref = "?aclAsUser_=dgcl.test.pref%40beta.gouv.fr"
 const viewAsAdmin = "?aclAsUser_=dgcl.test.admin%40beta.gouv.fr"
 
-const getUrl = (page) => `https://grist.numerique.gouv.fr/o/dgcl/${grist.docID}/p/${grist.pagesID[page]}`
-const getWidget = (page) => page.locator('iframe').contentFrame().locator('iframe').contentFrame()
+const getUrl = (page) => `https://grist.numerique.gouv.fr/o/dgcl/${grist.docID}/${grist.docName}/p/${grist.pagesID[page]}`
+const getWidgetHtmlBuilder = (page, number) => page.locator('iframe').contentFrame().locator('iframe').contentFrame()
 
-export default { getUrl, getWidget, viewAsPref, viewAsAdmin }
+export default { getUrl, getWidgetHtmlBuilder, viewAsPref, viewAsAdmin }
