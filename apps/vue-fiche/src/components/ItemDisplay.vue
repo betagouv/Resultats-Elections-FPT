768a9cc747f228e5bbca0d41169e76f699bfb0aa
Charline-L <charline.laporte@beta.gouv.fr>
2026-09-09 15:04:40 +0200

    Affiche le pourcentage depuis le composant

--- apps/vue-fiche/src/components/ItemDisplay.vue
+++ apps/vue-fiche/src/components/ItemDisplay.vue
@@ -3,27 +3,24 @@ import { computed } from 'vue'
 import valuesUtils from '@shared/utils/values.js'
 
 const props = defineProps(['data', 'currentRecord', 'tableColumnsInfos'])
-
 const value = computed(() => props.currentRecord[props.data])
 const isList = computed(() => value.value && typeof value.value === 'object' && value.value.length > 0)
-
-/* Display */
-const getPrettyLabel = (label) => {
-  const columnInfo = props.tableColumnsInfos.find(column => column.colId === label)
-  return columnInfo ? columnInfo.label : null
-}
-
+const columnInfo = computed(() => props.tableColumnsInfos.find(column => column.colId === props.data))
+const isPercent = computed(() => columnInfo.value ? valuesUtils.isPercent(columnInfo.value) : false)
+const label = computed(() => columnInfo.value?.label || null)
 const getPrettyValue = (value) => valuesUtils.prettify(value)
+const getPercentValue = (value) => valuesUtils.prettifyToPercent(value)
 </script>
 
 <template>
   <li class="fr-pb-0 fr-mb-1w" data-dgcl-testid="fiche-field-value">
     <div v-if="isList">
-      <p class="fr-mb-0" data-dgcl-testid="fiche-simple-value-label">{{ getPrettyLabel(data) }} :</p>
+      <p class="fr-mb-0" data-dgcl-testid="fiche-simple-value-label">{{ label }} :</p>
       <ul class="fr-mb-3w" data-dgcl-testid="fiche-simple-value-list">
         <li v-for="item in value" :key="item">{{ getPrettyValue(item) }}</li>
       </ul>
     </div>
-    <p v-else class="fr-mb-0" data-dgcl-testid="fiche-simple-value-single">{{ getPrettyLabel(data) }} : {{ getPrettyValue(value) }}</p>
+    <p v-else-if="isPercent" class="fr-mb-0" data-dgcl-testid="fiche-simple-value-single">{{ label }} : {{ getPercentValue(value) }}</p>
+    <p v-else class="fr-mb-0" data-dgcl-testid="fiche-simple-value-single">{{ label }} : {{ getPrettyValue(value) }}</p>
   </li>
 </template>
